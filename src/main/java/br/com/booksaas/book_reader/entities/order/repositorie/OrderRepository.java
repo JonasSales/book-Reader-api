ed27116d06dbc713e308134efc037c5ca3188637
Jonas Sales <jonassales510@gmail.com>
2026-01-25 14:19:43 -0300

    Creating new Query

--- src/main/java/br/com/booksaas/book_reader/entities/order/repositorie/OrderRepository.java
+++ src/main/java/br/com/booksaas/book_reader/entities/order/repositorie/OrderRepository.java
@@ -1,6 +1,8 @@
 package br.com.booksaas.book_reader.entities.order.repositorie;
 
 import br.com.booksaas.book_reader.entities.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -8,4 +10,9 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     boolean existsByUserId(Long userId);
 
+    boolean existsByIdAndUserId(Long bookId, Long id);
+
+    Page<Order> findAllByUserId(Pageable pageable, Long userId);
+
+    Long id(Long id);
 }
